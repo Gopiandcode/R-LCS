75558a37f0745327d291c94815108ab32258f9b5
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2018-02-06 12:48:40 +0000

    completed xcs-6input classifier

--- src/main/java/com/gopiandcode/lcs/Main.java
+++ src/main/java/com/gopiandcode/lcs/Main.java
@@ -16,7 +16,6 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         XCSBinaryClassifier xcs = new XCSBinaryClassifier();
 
-//        GraphingLogger logger = new AccuracyGraphingLogger("XCS Accuracy");
         CSVTrainingLogger logger = new CSVTrainingLogger("testData6.csv");
 
 
@@ -25,7 +24,7 @@ public class Main {
 
         SimpleBinaryClassifierTestRunner runner = new SimpleBinaryClassifierTestRunner(trainDataset, testDataset, xcs);
 
-        runner.setLogger(logger, 100);
+        runner.setLogger(logger, 10);
         runner.setShouldReset(true);
 
         runner.runTrainIterations(5000);
@@ -34,44 +33,6 @@ public class Main {
         System.out.println("Final Accuracy: " + runner.runTestIterations(1000));
 
         logger.close();
-    }
-//       RCSBinaryClassifier xcs = new RCSBinaryClassifier(3, 8);
-//
-//       // The graphing logger can customize graphs to only include the parameters you need - simply specify the members of the LoggableDataType enum you want in the final graph
-//        GraphingLogger logger = new MultiGraphingLogger("RCSBinaryClassifier Accuracy over Training", LoggableDataType.ACCURACY, LoggableDataType.POPULATION_SIZE);
-//
-//        BinaryClassifierDataset testDataset = LocalBinaryClassifierDataset.loadFromFile("testData6.txt");
-//        BinaryClassifierDataset trainDataset = LocalBinaryClassifierDataset.loadFromFile("trainData6.txt");
-//
-//        BinaryClassifierTestRunner runner = new RCSBinaryClassifierTestRunner(trainDataset, testDataset, xcs);
-//
-//        runner.setLogger(logger, 100);
-//        runner.setShouldReset(true);
-//
-//        runner.runTrainIterations(5000);
-//
-//
-//        configureGraph(xcs);
-//
-//        System.out.println("Final Accuracy: " + runner.runTestIterations(1000));
-//
-//        GraphRenderer renderer = new GraphRenderer(logger, 1280, 720,20);
-//        renderer.save("result6_rcs_6.png");
-//
-//        new RCSGraphGenerator(xcs).writeToDot("./examples/result_graph.dot");
-//        System.out.println("Finished");
-//    }
-
-    private static void configureGraph(RCSBinaryClassifier xcs) {
-        NumberAxis axis = new NumberAxis();
-        axis.setLabel("No of Population");
-        axis.setRange(0, xcs.getPopulationSize());
-        LoggableDataType.INTERMEDIATE_CLASSIFIER_COUNT.setRangeAxis(axis);
-        LoggableDataType.INTERMEDIATE_CLASSIFIER_COUNT.setSeriesPaint(Color.GREEN);
-        LoggableDataType.OUTPUT_CLASSIFIER_COUNT.setRangeAxis(axis);
-        LoggableDataType.OUTPUT_CLASSIFIER_COUNT.setSeriesPaint(Color.YELLOW);
-        LoggableDataType.POPULATION_SIZE.setRangeAxis(axis);
-        LoggableDataType.POPULATION_SIZE.setSeriesPaint(Color.BLUE);
     }
 
 }
